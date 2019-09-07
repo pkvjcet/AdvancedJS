e0ebd7ca235f3401d0149df4209bcc8a2c335b4a
pkvjcet <princekurian2000@gmail.com>
2019-09-07 15:41:03 +0530

    ajax using api json

--- ajaxdemo.js
+++ ajaxdemo.js
@@ -1,16 +1,16 @@
 $(document).ready( ()=>{
     $.ajax( {
         type:"GET",
-        url:"http://127.0.0.1:8080/mydata.json",
+        url:"https://jsonplaceholder.typicode.com/users",
         success:(response)=>{
             console.log(response);
-            var output="<table class='table'><tr><th>NAME</th><th>AGE</th><th>DOB</th><th>COLLEGE</th></tr>";
+            var output="<table class='table'><tr><th>NAME</th><th>USERNAME</th><th>EMAIL</th><th>Street</th></tr>";
             for(i in response){
                 var name=response[i].name;
-                var age=response[i].age;
-                var dob=response[i].dob;
-                var college=response[i].college;
-                output+="<tr><td>"+name+"</td><td>"+age+"</td><td>"+dob+"</td><td>"+college+"</td></tr>";
+                var username=response[i].username;
+                var email=response[i].email;
+                var street=response[i].address.street;
+                output+="<tr><td>"+name+"</td><td>"+username+"</td><td>"+email+"</td><td>"+street+"</td></tr>";
             }
             output+="</table>";
             $('#result').html(output);
