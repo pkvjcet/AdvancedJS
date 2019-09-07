4df35988d1f9f750eb909f7220f84bf83be8a11e
pkvjcet <princekurian2000@gmail.com>
2019-09-07 10:52:08 +0530

    jquery basics

---
+++ myscript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#mysubmitbutton").click(function(){
+        console.log("button clicked");
+    });
+});
+/*function readValues()
+{
+    var empid=document.getElementById('empid').Value;
+    var empname=document.getElementById('empname').Value;
+    var empaddress=document.getElementById('empaddress').Value;
+    var empmobile=document.getElementById('empmobile').Value;
+    var empsalary=document.getElementById('empsalary').Value;
+    var empdesignation=document.getElementById('empdesignation').Value;
+   
+}*/
